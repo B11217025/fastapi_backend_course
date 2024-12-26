32417a1c28a1c4c6daad73d8a7e071676dd89cc7
B11217025 <100165b824@gmail.com>
2024-12-26 15:27:32 +0800

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parents[1]))
+from app.database import Base
 from app.main import Base
 
 
