60a9a6a03f6a32f13460c4e24af2a784ba48d4bf
B11217025 <100165b824@gmail.com>
2024-12-26 15:33:31 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .routers import router
 
+# Initialize FastAPI
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
+# Initialize
+#Base.metadata.create_all(bind=engine)
 
+#Reister Router
 app.include_router(router=router, prefix="/api", tags=["todos"])
